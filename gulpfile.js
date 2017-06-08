d7eac590f83833e0aa316670fff88b8b4e5547a5
LIUG <adm@liug.co>
2017-06-08 02:44:47 -0300

    Delete pasta /assets

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,25 @@ const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 const cp = require('child_process');
 const clean = require('gulp-clean');
+const RevAll = require('gulp-rev-all');
+const awspublish = require('gulp-awspublish');
+const cloudfront = require("gulp-cloudfront");
+const rename = require('gulp-rename');
 const path = require('path');
 const runSequence = require('run-sequence');
 
 const srcPaths = {
   styl: 'themes/advec/_source/css/**/*.styl',
   js: 'themes/advec/_source/js/**/*.js',
-  jade: 'src/**/*.jade',
-  pug: 'src/**/*.pug',
+  jade: 'source/**/*.jade',
+  pug: 'source/**/*.pug',
   swig: 'themes/advec/layout/**/*.swig'
 
 };
 
 const buildPaths = {
-  css: 'public/assets/',
-  js: 'public/assets/'
+  css: 'dest/assets/',
+  js: 'dest/assets/'
 };
 
 // CSS MIN
@@ -59,12 +63,6 @@ gulp.task('js', () => {
     .pipe(gulp.dest(buildPaths.js))
 });
 
-// ASSETS COPY
-gulp.task('assets', () => {
-    return gulp.src(__dirname + '/assets/**')
-        .pipe(gulp.dest(__dirname + '/public/assets/'));
-});
-
 // IMG MIN
 gulp.task('img', function () {
     return gulp.src('public/assets/images/**/*')
@@ -73,6 +71,47 @@ gulp.task('img', function () {
             use: [pngquant()]
         }))
         .pipe(gulp.dest('public/assets/images'));
+});
+
+// REV
+gulp.task('rev', function () {
+  return gulp.src(['dest/**']) 
+    .pipe(RevAll.revision({ 
+        dontRenameFile: ['.html', '.xml', '.txt', '.eot', '.ttf', 'woff'],
+        prefix: 'https://assets.adv.ec/'
+    }))
+    .pipe(gulp.dest('public/'))  
+    .pipe(RevAll.manifestFile())
+    .pipe(gulp.dest('public/')); 
+});
+
+// AWS PUBLISH
+var aws = {
+  "params": {
+  "Bucket": "assets.adv.ec"
+  },
+  "distributionId": "E1SYAKGEMSK3OD"
+};
+
+gulp.task('aws', function() {
+  var publisher = awspublish.create({
+    params: {
+      Bucket: 'assets.adv.ec'
+    }
+  });
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+  };
+  return gulp.src('public/assets/**')
+    .pipe(rename(function(filePath) {
+        filePath.dirname = path.join('assets/', filePath.dirname);
+    }))
+    .pipe(awspublish.gzip())
+    .pipe(publisher.publish(headers))
+    .pipe(publisher.cache())
+    .pipe(publisher.sync())
+    .pipe(awspublish.reporter())
+    .pipe(cloudfront(aws));
 });
 
 // HEXO GENERATE
@@ -117,9 +156,9 @@ gulp.task('watch', function() {
 ///////////////////
 
 
-gulp.task('default', function(done) {
-    runSequence('css', 'js', 'assets', function() {
-        console.log('Funciaonando corretamente.');
+gulp.task('b', function(done) {
+    runSequence('css', 'js', 'rev', 'aws', function() {
+        console.log('Funcionando corretamente.');
         done();
     });
 });
