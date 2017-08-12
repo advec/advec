6851e337769042e0d3ca0e6a80deb8c82e8d88de
LIUG <adm@liug.co>
2017-08-12 01:40:30 -0300

    Alterando popup para sweet

--- themes/advec/_source/js/popup.js
+++ themes/advec/_source/js/popup.js
@@ -1,6 +1,8 @@
-$(".btn__popup--deposito").on( "click", function() {
-    $.sweetModal({
-	    title: 'HTML Content',
-	    content: 'You can place <b>anything</b> <i>you</i> want in here.'
+$(document).ready(function() {
+    $(".btn__popup--deposito").on( "click", function() {
+        $.sweetModal({
+            title: 'HTML Content',
+            content: 'You can place <b>anything</b> <i>you</i> want in here.'
+        });
     });
 });
