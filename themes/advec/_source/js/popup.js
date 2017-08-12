d3844af938ce0b3f04bc33bf0d1f5d6dfe532b93
LIUG <adm@liug.co>
2017-08-12 01:48:57 -0300

    Alterando sweetmodal

--- themes/advec/_source/js/popup.js
+++ themes/advec/_source/js/popup.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-    $(".btn__popup--deposito").on( "click", function() {
-        $.sweetModal({
-            title: 'HTML Content',
-            content: 'You can place <b>anything</b> <i>you</i> want in here.'
-        });
-    });
-});
+				var variants = {
+					'btn__popup--deposito': {
+						args: [
+							{
+								title: 'HTML Content',
+								content: 'You can place <b>anything</b> <i>you</i> want in here.'
+							}
+						]
+					}
+				};
+				for (var key in variants) {
+					if (variants.hasOwnProperty(key)) {
+						var variant = variants[key];
+						$('.' + key).on('click', { variant: variant }, function(e) {
+							var variant = e.data.variant;
+							variant.fn = variant.fn || $.sweetModal;
+							variant.fn.apply(this, variant.args);
+						});
+					}
+				}
+			});
