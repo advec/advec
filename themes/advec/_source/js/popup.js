f4c07d4c1556aa3590104d1eeebe76f5c5b9e691
LIUG <adm@liug.co>
2017-08-12 01:24:18 -0300

    Alterando popup para swetmodal

--- themes/advec/_source/js/popup.js
+++ themes/advec/_source/js/popup.js
@@ -1,25 +1,6 @@
-// Get the modal
-var popup = document.querySelector('.popup__deposito')
-
-// Get the button that opens the modal
-var btn = document.querySelector('.btn__popup--deposito');
-
-// Get the <span> element that closes the modal
-var span = document.querySelector('.popup__content__close');
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    popup.style.display = "flex";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    popup.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
-}
+$(".btn__popup--deposito").on( "click", function() {
+    $.sweetModal({
+	    title: 'HTML Content',
+	    content: 'You can place <b>anything</b> <i>you</i> want in here.'
+    });
+});
